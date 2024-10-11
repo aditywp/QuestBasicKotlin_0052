6d5ca603e61d46b28713fc8a55480d19b4b0530a
adityapermono8 <adityapermono8@gmail.com>
2024-10-11 14:41:32 +0700

    membuat if-else memeriksa apakah number lebih besar dari 0 dan mencetak "Positive Number" jika benar dan mencetak "Negative Number" jika < 0

--- app/src/main/java/com/example/basickotlin/ConditionalExpressions.kt
+++ app/src/main/java/com/example/basickotlin/ConditionalExpressions.kt
@@ -5,5 +5,13 @@ package com.example.basickotlin
 // Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
 fun ConditionalStatement() {
     println("=== Conditional Expression ===")
-    
+
+// If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
 }
