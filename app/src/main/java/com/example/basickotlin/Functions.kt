5f3a54cce8f0dc1c8683fd80fa1b60db3ad9e541
adityapermono8 <adityapermono8@gmail.com>
2024-10-11 15:30:01 +0700

    memangil fungsi dengan parameter dengan variabel name

--- app/src/main/java/com/example/basickotlin/Functions.kt
+++ app/src/main/java/com/example/basickotlin/Functions.kt
@@ -4,3 +4,9 @@ fun withoutParameter() {
     println("== without Parameter ==")
     println("Hello, World!")
 }
+
+fun withParameter (name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
