1833c21947a5191f47c04daa604bfc32e195f824
adityapermono8 <adityapermono8@gmail.com>
2024-10-11 14:32:41 +0700

    membuat fungsi main untuk class contact yang diberikan nilai lalu mencetak email

--- app/src/main/java/com/example/basickotlin/Class.kt
+++ app/src/main/java/com/example/basickotlin/Class.kt
@@ -3,3 +3,10 @@ package com.example.basickotlin
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+// Prints the value of the property: email
+    println(contact.email)
+}
